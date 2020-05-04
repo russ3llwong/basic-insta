2c1e9171b220a0689e1b534cbdec917eee63cbdc
russ3llwong <russellwong97@hotmail.com>
2020-05-03 17:30:01 -0700

    Reinitialized project

---
+++ build/precache-manifest.ef8a092539eae82697dfc1709d0a7ef1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7bc23724b5bc1807d450",
+    "url": "/static/js/main.7bc23724.chunk.js"
+  },
+  {
+    "revision": "0b1b931ce662c976408f",
+    "url": "/static/js/2.0b1b931c.chunk.js"
+  },
+  {
+    "revision": "7bc23724b5bc1807d450",
+    "url": "/static/css/main.2e1296fb.chunk.css"
+  },
+  {
+    "revision": "3c60677cf5b3dd46c2d00658f45665f7",
+    "url": "/index.html"
+  }
+];
